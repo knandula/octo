c6d4794545a56e011b620243a8bb60ee09c367d8
krishnanandula <nandu.krishna@hotmail.com>
2014-11-01 14:13:10 +0100

    setting up the UI

--- server.js
+++ server.js
@@ -36,22 +36,13 @@ db.once('open',function callback(){
     console.log("fiction tree database opened..")
 });
 
-var messageSchema = mongoose.Schema({message:String});
-var Message = mongoose.model('Message',messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err,messageDoc){
-    mongoMessage = messageDoc.message;
-});
-
 app.use(express.static(__dirname + '/public'))
 app.get('/partials/:partialPath',function(req,res){
     res.render('partials/' + req.params.partialPath);
 })
 
 app.get('*',function(req,res){
-    res.render('index',{
-        mongoMessage: mongoMessage
-    });
+    res.render('index');
 });
 
 var port = process.env.PORT || 3030;
